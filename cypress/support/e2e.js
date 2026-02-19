05ec24cacb289b6012c55e1dc216821a0c6e4fef
SaiHemanthMaddi <maddi.saihemanth938@gmail.com>
2026-02-19 14:00:38 +0200

    feat: hide XHR and Request logs in Cypress UI

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -4,3 +4,11 @@ import '@testing-library/cypress/add-commands';
 
 
 Cypress.on('uncaught:exception', () => false);
+
+const app = window.top;
+if (!app.document.head.querySelector("[data-hide-command-log-request]")) {
+    const style = app.document.createElement("style");
+    style.innerHTML = ".command-name-request, .command-name-xhr { display: none }";
+    style.setAttribute("data-hide-command-log-request", "");
+    app.document.head.appendChild(style);
+}
